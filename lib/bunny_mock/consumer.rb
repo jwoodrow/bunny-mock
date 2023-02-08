8d26a28c529a821e92badd2d18a3ec2fa58fbbf5
James Woodrow <james.woodrow@mooncard.co>
2023-02-08 17:21:35 +0100

    remove consumer from consumers when cancel is called

--- lib/bunny_mock/consumer.rb
+++ lib/bunny_mock/consumer.rb
@@ -1,10 +1,19 @@
 module BunnyMock
   class Consumer
-    attr_reader :block, :args
+    attr_reader :block, :args, :queue
 
-    def initialize(args, block)
+    def initialize(args, queue, block)
       @args = args
+      @queue = queue
       @block = block
+    end
+
+    def cancel
+      current_consumers = queue.instance_variable_get('@consumers')
+      queue.instance_variable_set('@consumers', current_consumers - [self])
+      true
+    rescue Exception
+      false
     end
   end
 end
